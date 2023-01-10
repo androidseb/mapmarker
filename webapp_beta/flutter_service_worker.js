e7e20d8f642172dd6fc4ba5912ca2f7638dd9d71
androidseb <theandroidseb@gmail.com>
2023-01-11 00:59:14 +0100

    Updated web app beta to version 3.0.0-beta-495+495

--- webapp_beta/flutter_service_worker.js
+++ webapp_beta/flutter_service_worker.js
@@ -7,8 +7,8 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
 "canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
-"index.html": "0fde178a8168dd8143965c917a1ea5af",
-"/": "0fde178a8168dd8143965c917a1ea5af",
+"index.html": "eafb2e662c93d971cb3a8e811d4454e7",
+"/": "eafb2e662c93d971cb3a8e811d4454e7",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "icons/Icon-192.png": "3c328cfe2fa69d19b6bb97ed8a9476fd",
 "icons/Icon-512.png": "1f4d3a3b30ec92695adfc87857c068c5",
@@ -652,9 +652,9 @@ const RESOURCES = {
 "assets/AssetManifest.json": "b7b752080adbb7c02e5826e67e233a95",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
 "manifest.json": "352cadf8e40066ac1773ebbc922f0722",
-"main.dart.js": "4b12babda68795185891eca9fbbc35b3",
+"main.dart.js": "d8c38e89c6b9e729a56804baeb717ffd",
 "favicon.png": "08b885e38ecdf53b6a25c93bbbf679ba",
-"version.json": "7a07459dcbb32b6cdf37bbe95936b96e"
+"version.json": "48745c88a5ab02dbd0ab59bd1a83f575"
 };
 
 // The application shell files that are downloaded before a service worker can
