6771c46e28e902c6720f8ef75e460ac0f1e5b969
androidseb <theandroidseb@gmail.com>
2024-06-11 06:51:40 +0200

    Updated webapp_beta to version 3.9.0_723

--- webapp_beta/flutter_service_worker.js
+++ webapp_beta/flutter_service_worker.js
@@ -9,7 +9,7 @@ const RESOURCES = {"index.html": "a2144a12daf3aa5c8fbc18af8f273806",
 "icons/Icon-maskable-512.png": "1e2a172095fc13520c7aa906543038fb",
 "icons/Icon-192.png": "3c328cfe2fa69d19b6bb97ed8a9476fd",
 "icons/Icon-maskable-192.png": "205fcad2f4dbee4698f90b92a1bb1573",
-"flutter_bootstrap.js": "de103a8e6efcc55d47fd51da934cee3f",
+"flutter_bootstrap.js": "f43d5bc0c1e0febab021176208a58ba0",
 "canvaskit/canvaskit.js.symbols": "74a84c23f5ada42fe063514c587968c6",
 "canvaskit/skwasm.wasm": "4051bfc27ba29bf420d17aa0c3a98bce",
 "canvaskit/chromium/canvaskit.js.symbols": "ee7e331f7f5bbf5ec937737542112372",
@@ -2636,10 +2636,10 @@ const RESOURCES = {"index.html": "a2144a12daf3aa5c8fbc18af8f273806",
 "assets/AssetManifest.bin": "44363b0d820b3bee050f01c4954ee0c1",
 "assets/AssetManifest.bin.json": "94bc62e11b7cfd3517c81c647742ad6f",
 "manifest.json": "2aed15af34ca38d309f25b7ad208b6ea",
-"version.json": "f15ffe877c43c7fdced76fcd003c3141",
+"version.json": "2d0a45d7c379699aff80e8f79b39d5fc",
 "favicon.png": "08b885e38ecdf53b6a25c93bbbf679ba",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"main.dart.js": "2ab123dfbf39bfb15b79a976aab13c6c"};
+"main.dart.js": "c7f2624cf054aca75507919badff9e0c"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
