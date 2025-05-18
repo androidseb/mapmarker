71c7f4174e1f3eeb708133a6944c587a73ad1311
androidseb <theandroidseb@gmail.com>
2025-05-18 14:55:06 +0200

    Updated webapp_beta to version 3.12.0_755_beta

--- webapp_beta/flutter_bootstrap.js
+++ webapp_beta/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1393690618"
+    serviceWorkerVersion: "1265872474"
   }
 });
