8e94290c5ffcc7586084bb866888efec7d610b10
androidseb <theandroidseb@gmail.com>
2026-09-21 22:19:44 +0200

    Updated webapp_beta to version 3.21.0_851_beta

--- webapp_beta/flutter_bootstrap.js
+++ webapp_beta/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3891430334" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2867047248" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
